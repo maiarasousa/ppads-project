2cb3d5e35a7649ffd44d73a250274b5ffb249f4c
Maiara Sousa <maiara_sousa10@hotmail.com>
2024-04-01 22:46:30 -0300

    Delete repository directory

--- repository/db_integration.py
+++
@@ -1,9 +0,0 @@
-import pyodbc
-
-server = 'DESKTOP-JV5AKTF'
-database = 'SYSTEM_PRESENCE'
-username = 'sa'
-password = 'aplicando@2024guktdhg'
-
-conexao = pyodbc.connect('DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
-print("Conexão bem-sucedida!")
